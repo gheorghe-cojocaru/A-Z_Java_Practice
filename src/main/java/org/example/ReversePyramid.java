6484e7ecdbb989356eead90dafa448ae5c6cba7a
Gheorghe-Cojocaru <gheorghe.cojocaru@hotmail.com>
2024-07-23 22:14:22 +0200

    commit

---
+++ src/main/java/org/example/ReversePyramid.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class ReversePyramid {
+    public static void main(String[] args) {
+        int number = 7;
+
+        int i = number, j;
+
+        while (i > 0) {
+            j = 0;
+
+            while (j++ < number - i) {
+                System.out.print(" ");
+            }
+
+            j = 0;
+
+            while (j++ < (i * 2) - 1) {
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            i--;
+        }
+    }
+}
