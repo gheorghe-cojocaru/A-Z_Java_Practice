95273f2eef188acd3e4474ace4932c056ca04468
Gheorghe-Cojocaru <gheorghe.cojocaru@hotmail.com>
2024-08-25 09:47:52 +0200

    commit

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,25 +1,23 @@
 package org.example;
 
-
 public class Test {
     public static void main(String[] args) {
-      int row = 1, column = 1;
-      int x;
-      do {
-          x = 4;
-          do {
-              System.out.print("");
-              x--;
-          } while (x >= row);
-          column = 1;
-          do {
-              System.out.print(column + " ");
-              column++;
-          } while (column <= 5);
-          System.out.println(" ");
-          row++;
-      } while (row <= 5);
 
+      int[] marks = {123, 125, 33, 544, 223, 98};
+
+      int highestMarks = maximum(marks);
+        System.out.println("Die grösste Zahl ist: " + highestMarks);
+
+    }
+    public static int maximum(int[] numbers) {
+        int maxOfArr = numbers[0];
+
+        for (int num : numbers) {
+            if (num > maxOfArr) {
+                maxOfArr = num;
+            }
+        }
+        return maxOfArr;
     }
 
 }
