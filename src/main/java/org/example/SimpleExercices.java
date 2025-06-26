9636b173afdfa193817b77191bd678fb885e6bed
Georg <gheorghe.cojocaru@hotmail.com>
2025-06-26 21:42:18 +0200

    commit

---
+++ src/main/java/org/example/SimpleExercices.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class SimpleExercices {
+    public static void main(String[] args) {
+
+      /*  String[] farben = {"Rot", "Grün", "Blau", "Lila", "Beige"};
+
+        for (String farbe : farben) {
+            System.out.println("Farbe: " + farbe); */
+
+
+          /*  ArrayList<String> staedte = new ArrayList<>();
+            staedte.add("Berlin");
+            staedte.add("Frankfurt");
+            staedte.add("Bonn");
+
+            staedte.add("Wöllstein");
+
+            System.out.println("Städte: " + staedte); */
+
+        int a = 5;
+        int b = 14;
+
+        System.out.println("Vorher: a = " + a + " b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("Nacher: a = " + a + " b= " + b);
+        }
+    }
+
